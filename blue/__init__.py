3154f0098f9696cd48536599413659e47747491f
Igor Santos <igorbpf@gmail.com>
2017-01-11 13:25:04 -0200

    Add api [2]

--- blue/__init__.py
+++ blue/__init__.py
@@ -6,4 +6,4 @@ from blue.site.routes import mod
 from blue.api.routes import mod
 
 app.register_blueprint(site.routes.mod)
-app.register_blueprint(api.routes.mod)
+app.register_blueprint(api.routes.mod, url_prefix='/api')
