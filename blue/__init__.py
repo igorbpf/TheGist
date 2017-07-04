b748779f67c2864b4eea448042669144b649d090
Igor Santos <igorbpf@gmail.com>
2017-07-03 21:50:31 -0300

    Add new config vars

--- blue/__init__.py
+++ blue/__init__.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ['MAIL_SENDER']
-app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
-app.config['MAIL_DEFAULT_SENDER'] = os.environ['MAIL_SENDER']
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ['MAIL_SENDER']
+# app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+# app.config['MAIL_DEFAULT_SENDER'] = os.environ['MAIL_SENDER']
 
 # Enable cors
 CORS(app)
--- config.py
+++ config.py
@@ -12,6 +12,13 @@ class StagingConfig(Config):
     BROKER_URL = os.environ['REDIS_URL']
     CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 
+    MAILGUN_DOMAIN = os.environ['MAILGUN_DOMAIN']
+    MAILGUN_PUBLIC_KEY = os.environ['MAILGUN_PUBLIC_KEY']
+    MAILGUN_SMTP_LOGIN = os.environ['MAILGUN_SMTP_LOGIN']
+    MAILGUN_SMTP_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+    MAILGUN_SMTP_PORT = os.environ['MAILGUN_SMTP_PORT']
+    MAILGUN_SMTP_SERVER = os.environ['MAILGUN_SMTP_SERVER']
+
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
