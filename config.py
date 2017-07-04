99a1047618a7cea01bba2851a6c3ddfe92b54297
Igor Santos <igorbpf@gmail.com>
2017-07-03 22:20:16 -0300

    Fix config vars [2]

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ class StagingConfig(Config):
     BROKER_URL = os.environ['REDIS_URL']
     CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 
-    MAIL_DEFAULT_SENDER = os.environ['MAILGUN_DOMAIN']
+    MAIL_DEFAULT_SENDER = os.environ['MAILGUN_SMTP_LOGIN']
     #MAILGUN_PUBLIC_KEY = os.environ['MAILGUN_PUBLIC_KEY']
-    MAIL_USERNAME = os.environ['MAILGUN_SMTP_LOGIN']
+    MAIL_USERNAME = os.environ['MAILGUN_DOMAIN']
     MAIL_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
     MAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
     MAIL_SERVER = os.environ['MAILGUN_SMTP_SERVER']
     MAIL_USE_TLS = True
-    
+
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
